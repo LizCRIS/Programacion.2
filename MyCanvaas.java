06c347bd158b6f0b814b08a4df63850ce58deff4
LizCRIS <89422638+LizCRIS@users.noreply.github.com>
2022-10-05 14:08:43 -0500

    Add files via upload

--- MyCanvaas.java
+++ MyCanvaas.java
@@ -1,19 +1,20 @@
 import javax.swing.JPanel;
+import java.awt.Dimension;
 import java.awt.Graphics;
-import javax.swing.plaf.DimensionUIResource;
+import java.awt.event.*;
 
-public class MyCanvaas extends JPanel {
+public class MyCanvaas extends JPanel implements KeyListener, ActionListener {
     Point pSun = new Point(Confiig.WINDOW_W / 2, Confiig.WINDOW_H / 2);
 
     public MyCanvaas() {
-        setPreferredSize(new DimensionUIResource(Confiig.WINDOW_W, Confiig.WINDOW_H));
+        setPreferredSize(new Dimension(Confiig.WINDOW_W, Confiig.WINDOW_H));
         setBackground(Confiig.COLOR_BG);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Paintbruush paintBruush = new Paintbruush(g);
+        PaintBruush paintBruush = new PaintBruush(g);
         paintBruush.drawSky();
         paintBruush.drawMountains();
         paintBruush.drawLake();
@@ -21,9 +22,46 @@ public class MyCanvaas extends JPanel {
         paintBruush.drawTree(110, 50);
         paintBruush.drawTree(270, 50);
         paintBruush.drawTree(30, 50);
-        // paintBruush.drawSun(pSun);
+        paintBruush.drawSun(pSun);
         paintBruush.drawOvni();
         paintBruush.drawPlane();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent event) {
+
+        int key = event.getKeyCode();
+        if (key == KeyEvent.VK_UP) {
+            pSun.setY(pSun.getY() - Confiig.SUN_STEP);
+        }
+        if (key == KeyEvent.VK_DOWN) {
+            pSun.setY(pSun.getY() + Confiig.SUN_STEP);
+        }
+        if (key == KeyEvent.VK_RIGHT) {
+            pSun.setX(pSun.getX() + Confiig.SUN_STEP);
+        }
+        if (key == KeyEvent.VK_LEFT) {
+            pSun.setX(pSun.getX() - Confiig.SUN_STEP);
+        }
+        repaint();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+
     }
 
 }
